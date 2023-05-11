1009fa24e5d00a1c1d339cbfefe23eae86c4c9b3
yaofanghao <yaofanghao7@163.com>
2023-05-11 21:03:16 +0800

    2023.5.11 update

---
+++ 剑指Offer学习记录/04-二维数组查找.c
@@ -0,0 +1,41 @@
+/*
+ * @Author: yao fanghao
+ * @Date: 2023-05-11 17:46:44
+ * @LastEditTime: 2023-05-11 20:56:47
+ * @LastEditors: yao fanghao
+ */
+// https://leetcode.cn/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+ * 输入 **matrix 是长度为 matrixSize 的数组指针的数组，其中每个元素（也是一个数组）
+ * 的长度组成 *matrixColSize 数组作为另一输入，*matrixColSize 数组的长度也为 matrixSize
+ */
+bool findNumberIn2DArray(int** matrix, int matrixSize, int* matrixColSize, int target){
+    if (matrix == NULL|| matrixSize == 0 || *matrixColSize == 0)  return false;
+
+    int i=0, j=*matrixColSize-1;
+    while(i<matrixSize && j>=0)
+    {
+        if(matrix[i][j]==target)  return true;
+        else if(matrix[i][j]>target)  j--;
+        else i++;
+    }
+    return false;
+}
+
+int main()
+{
+    int matrix[5][5] = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+    int matrixSize = sizeof(matrix[0]);
+    int matrixColSize = sizeof(matrix[0][0]);
+    
+    printf("matrixSize=%d\n",matrixSize);
+    printf("matrixColSize=%d\n", matrixColSize);
+
+    int target = 5;
+
+    return 0;
+}
