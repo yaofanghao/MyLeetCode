f8fc10df81fd55aac6188b7fb40a8a93ebc2a856
yaofanghao <yaofanghao7@163.com>
2024-01-10 21:21:38 +0800

    2024.1.10 update

--- LeetCode重刷/common.cpp
+++ LeetCode重刷/common.cpp
@@ -1,114 +1,30 @@
-
-// acm模式输入测试
-
+/*
+ * @Author: yao fanghao
+ * @Date: 2023-08-17 11:42:01
+ * @LastEditTime: 2023-09-23 17:02:01
+ * @LastEditors: yao fanghao
+ */
 #include <iostream>
 #include <string>
 #include <vector>
-#include "common.h"
-
+#include <sstream>
 using namespace std;
 
-void input_array(){
-    cout << "测试-输入不确定个数的多个数字" << endl;    
-    
-    vector<int> nums;
-    int num;
-    while(cin >> num) {
-        nums.push_back(num);
-        if(getchar() == '\n') {
+vector<int> split(std::string line){
+    vector<int> numbers;
+    while (std::getline(cin, line)) {
+        if (line.empty()) {
             break;
+        }        
+        stringstream ss(line);
+        string numStr;
+        while (std::getline(ss, numStr, ',')) {
+            int num = std::stoi(numStr);
+            numbers.push_back(num);
         }
     }
-
-    for(int i = 0; i < nums.size(); i++) {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-}
-
-void input_matrix(){
-    cout << "测试-输入m行n列的矩阵" << endl;    
     
-    int m, n;
-    cin >> m >> n;
-    vector<vector<int>> matrix(m, vector<int>(n));
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                cin >> matrix[i][j];
-            }
-        }
-
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void input_string(){    
-    cout << "测试-输入字符串" << endl;  
-
-    string s;
-    getline(cin, s);
-
-    cout << s << endl;
-}
-
-void input_strings(){
-    cout << "测试-输入不确定个数的多个字符串" << endl;  
-
-    vector<string> strings;
-    string str;
-    while(cin >> str) {
-        strings.push_back(str);
-        // 读到换行符，终止循环
-        if(getchar() == '\n') {
-            break;
-        }
-    }
-
-    // 验证是否读入成功
-    for(int i = 0; i < strings.size(); i++) {
-        cout << strings[i] << " ";
-    }
-    cout << endl;
-}
-
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int _val):val(_val),next(nullptr){}
-    ListNode(int _val,ListNode* _next):val(_val),next(_next){}
-};
-
-void input_list_node(){
-    cout << "测试-输入不确定长度的链表， 输入-1时表示退出输入" << endl;  
-
-    ListNode* LHead = new ListNode(-1);
-    ListNode* pre = LHead;
-    ListNode* cur = nullptr;
-    
-    int num;
-    while(cin >> num)
-    {
-        // 如果-1，退出输入链表数字
-        if(num == -1) break;
-        cur = new ListNode(num);
-        pre->next = cur;
-        pre = cur;
-    }    
-    cur = LHead->next;
-    
-    while(cur)
-    {
-        cout << cur->val << " ";
-        cur = cur->next;
-
-    }
-    
-    cout << endl;
+    return numbers;
 }
 
 void printVector(vector<int>& nums)
@@ -118,4 +34,24 @@ void printVector(vector<int>& nums)
         cout << *it << " ";
     }
     cout<<endl;    
+}
+
+int main() {
+    std::string line; 
+    vector<int> res = split(line);    
+    int towersLength = res[0];
+    int radius = res[1];
+    // printVector(res);
+    
+    int max=0;
+    int max_index=0;
+    for(int i=0; i<towersLength; i++){
+        if(res[4+3*i]>=max){
+            max_index = 4+3*i;
+            max = res[4+3*i];
+        }        
+    }
+    // cout<<"max_index"<<max_index<<endl;    
+    cout<<res[max_index-2]<<","<<res[max_index-1]<<endl;
+    return 0;
 }
--- LeetCode重刷/test.cpp
+++ LeetCode重刷/test.cpp
@@ -1,55 +1,37 @@
 /*
  * @Author: yao fanghao
  * @Date: 2023-08-20 14:31:52
- * @LastEditTime: 2023-08-20 14:36:25
+ * @LastEditTime: 2023-09-23 16:44:54
  * @LastEditors: yao fanghao
  */
 #include <iostream>
+#include <string>
 #include <vector>
-using namespace std;
-
-void printVector(vector<double>& nums)
-{
-    for(vector<double>::iterator it = nums.begin(); it != nums.end(); it++)
-    {
-        cout << *it << " ";
-    }
-    cout<<endl;    
-}
+#include <sstream>
 
 int main() {
-    vector<double> nums;
-    double num;
-    while(cin >> num) {
-        nums.push_back(num);
-        if(getchar() == '\n') {
+    std::vector<int> numbers;
+    std::string line;
+    
+    // 读取多行输入直到遇到空行
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) {
             break;
         }
-    }
-
-
-    printVector(nums);
-
-    if(nums.size()<=1){
-        return 1;
-    }
-
-    int maxLen=1;
-    for(int i=1; i<nums.size(); i++){
-        if(nums[i]-nums[i-1]>0){
-            maxLen++;
-            cout<<"nums i="<<nums[i]<<endl;
-            cout<<maxLen<<endl;
+        
+        // 使用 stringstream 和 getline 将每行的数字以逗号为分隔符拆分
+        std::stringstream ss(line);
+        std::string numStr;
+        while (std::getline(ss, numStr, ',')) {
+            int num = std::stoi(numStr);
+            numbers.push_back(num);
         }
-        else{
-            maxLen = 1;
-        }
-
     }
-
-    cout<<maxLen<<endl;
-
-    return maxLen;
-
+    
+    // 输出数字
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    
+    return 0;
 }
-// 64 位输出请用 printf("%lld")
